a55a8916ad047576643bad97a02cee90c01c9e80
Kykeyn <Kykeyn@gmail.com>
2024-07-23 18:38:22 +0300

    Update Homework2.py

--- Homework2.py
+++ Homework2.py
@@ -2,5 +2,5 @@ Count_completed_HW = 12
 Count_hours_spend = 1.5
 Course_name = 'Python'
 Time_per_task = (Count_hours_spend / Count_completed_HW)
-print("Курс:" + Course_name + ","" всего задач:"+ str(Count_completed_HW) +", затрачено часов:"+str(Count_hours_spend)+
-      ", среднее время выполнения:"+str(Time_per_task),"часа")
+print("Курс: " + Course_name + ","" всего задач: "+ str(Count_completed_HW) +", затрачено часов: "+str(Count_hours_spend)+
+      ", среднее время выполнения: "+str(Time_per_task),"часа")
