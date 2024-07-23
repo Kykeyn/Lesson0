8652ae309c01e7a48ca416ecdd9adcd0ec94aa5b
Kykeyn <Kykeyn@gmail.com>
2024-07-23 17:47:02 +0300

    Add files via upload

---
+++ resolve1.py
@@ -0,0 +1,25 @@
+#Task 1
+a = 'Курс: Python , всего задач: 12 , затрачено часов: 1.5 , среднее время выполнения: 0.125 часа'
+print(len (a))
+#Task 2
+First = 8
+Second = 9
+Summa = First + Second
+Diff = First - Second
+print(Summa,Diff)
+#Task 3
+First = 9
+Second = 5
+Third = 3
+Mean = (First + Second + Third) / 3
+print(Mean)
+#Task 4
+First_String = 'Вторник'
+Second_String = 'Понедельник'
+print(Second_String + ', ' + First_String)
+#Task 5
+a = 6
+b = 9
+c = 3
+f = (a+b)+(a+c)
+print(f**3/2)
