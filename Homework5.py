99420267b32a9cd3d195ba765368a215504df9cf
Kykeyn <Kykeyn@gmail.com>
2024-07-25 18:33:09 +0300

    Add files via upload

---
+++ Homework5.py
@@ -0,0 +1,8 @@
+immutable_var = 'string',True,1,1.5
+print(immutable_var)
+# immutable_var [3]=5
+# print(immutable_var)
+print('immutable_var не поддерживает обращение по элементам')
+mutable_list = ['string',True,1,1.5]
+mutable_list [0]=1
+print(mutable_list)
