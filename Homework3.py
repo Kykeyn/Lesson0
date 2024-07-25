9e78dbbf1b1bd903384033d543639651ab51edae
Kykeyn <Kykeyn@gmail.com>
2024-07-25 19:59:08 +0300

    Update Homework3.py

--- Homework3.py
+++ Homework3.py
@@ -2,6 +2,7 @@ name = 'Александр'
 print('Name: ' + name)
 age = 27
 print('Age: '+str(age))
-print('New Age: ' + str(age+1))
+age = age + 1
+print('New Age:',age)
 is_student = True
 print('Is Student: ' + str(is_student))
